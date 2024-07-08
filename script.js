5761b65b43d0a7fe15393dd9942f3399da83dcec
Max Lazaro <maxlazaromax@gmail.com>
2024-07-08 16:52:50 -0300

    salvando

---
+++ script.js
@@ -0,0 +1,84 @@
+
+
+// Instruções para entrega
+//  # 2️⃣ Calculadora de partidas Rankeadas
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+// - Funções
+
+// ## Objetivo:
+
+// Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+
+// depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+// Se vitórias for menor do que 10 = Ferro
+// Se vitórias for entre 11 e 20 = Bronze
+// Se vitórias for entre 21 e 50 = Prata
+// Se vitórias for entre 51 e 80 = Ouro
+// Se vitórias for entre 81 e 90 = Diamante
+// Se vitórias for entre 91 e 100= Lendário
+// Se vitórias for maior ou igual a 101 = Imortal
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+// "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+
+
+// Bons estudos 😉
+
+
+// ==================================================================================================
+
+
+
+
+// function receber(vitorias, derrotas) {
+//     let somar = vitorias - derrotas;
+//     return somar;
+// }
+
+
+// function determinarNivel(vitorias) {
+//     if (vitorias < 10) {
+//         return 'Ferro';
+//     } else if (vitorias >= 11 && vitorias <= 20) {
+//         return 'Bronze';
+//     } else if (vitorias >= 21 && vitorias <= 50) {
+//         return 'Prata';
+//     } else if (vitorias >= 51 && vitorias <= 80) {
+//         return 'Ouro';
+//     } else if (vitorias >= 81 && vitorias <= 90) {
+//         return 'Diamante';
+//     } else if (vitorias >= 91 && vitorias <= 100) {
+//         return 'Lendário';
+//     } else {
+//         return 'Imortal';
+//     }
+// }
+
+
+// let vitorias = 100;
+// let derrotas = 5;
+// let resultados = receber(vitorias, derrotas);
+// let nivel = determinarNivel(vitorias);
+
+// console.log(`O Herói tem de saldo ${resultados} está no nível de ${nivel}`);
+
+
+
+
+
+
+
+
+
+
+
+
